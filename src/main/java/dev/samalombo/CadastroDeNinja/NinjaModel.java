30674aeae9fe1a270ef34253a96e58b0f4374d02
Antonio <tonygabriel74@gmail.com>
2026-04-27 07:47:55 -0300

    Arquivo NinjaModel.java atualizado

--- src/main/java/dev/samalombo/CadastroDeNinja/NinjaModel.java
+++ src/main/java/dev/samalombo/CadastroDeNinja/NinjaModel.java
@@ -10,16 +10,26 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private int idade;
     private String email;
+    private int idade;
+
 
     public NinjaModel() {
     }
 
-    public NinjaModel(String nome, int idade, String email) {
+    public NinjaModel(Long id, String nome, String email, int idade) {
+        this.id = id;
         this.nome = nome;
-        this.idade = idade;
         this.email = email;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -30,19 +40,19 @@ public class NinjaModel {
         this.nome = nome;
     }
 
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
