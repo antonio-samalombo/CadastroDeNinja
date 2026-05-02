b75147f3de4a18774deac9f9bf3df15760c92634
Antonio <tonygabriel74@gmail.com>
2026-05-02 16:08:10 -0300

    feat: Coloca lombok na classe para abstracao

--- src/main/java/dev/samalombo/CadastroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/samalombo/CadastroDeNinja/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.samalombo.CadastroDeNinja.Ninjas;
 
 import dev.samalombo.CadastroDeNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -21,46 +27,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //Foreing Key ou Chave Estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
