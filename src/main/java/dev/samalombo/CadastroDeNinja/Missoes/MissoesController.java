ad88c6c7e98b26adb6a841e3318bf1b9bcafa2b0
Antonio <tonygabriel74@gmail.com>
2026-05-19 17:30:34 -0300

    feat: Cria as rotas do MissoesController com o mock

--- src/main/java/dev/samalombo/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/dev/samalombo/CadastroDeNinja/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.samalombo.CadastroDeNinja.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("listar")
+    public String listarMissao(){
+        return "Missões listadas com sucesso ";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("deletar")
+    public String deletarMissao(){
+        return "Missão deltada com sucesso";
+    }
 }
