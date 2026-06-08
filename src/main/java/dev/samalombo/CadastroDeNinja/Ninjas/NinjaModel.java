075c2470b63f864277ce23a1d69a789365990705
Antonio <tonygabriel74@gmail.com>
2026-06-08 16:05:28 -0300

    refactor: Inclui a annotation ToString nos models de Ninja e Missoes

--- src/main/java/dev/samalombo/CadastroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/samalombo/CadastroDeNinja/Ninjas/NinjaModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")// ToString: Para transformar lugar em memoria em valor em memoria e exclude para evitar erro de serialização
 public class NinjaModel {
 
     @Id
